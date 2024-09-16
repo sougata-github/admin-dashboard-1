f4c5f461c9853045949d38fb196bdef58d73cfd4
Sougata <sougatadas9874@gmail.com>
2024-09-16 21:20:18 +0530

    set up project, add routes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dashboard",
+  title: "Admin Dashboard",
   description: "Admin Dashboard built using Next.js.",
 };
 
